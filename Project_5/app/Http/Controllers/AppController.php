8dedca39f0ca5fa855f161b09c9d9a0ab715d2aa
AhmedAlKharraz <64503987+AhmedAlKharraz@users.noreply.github.com>
2020-06-09 23:21:03 +0400

    Second update
    Soved the issue in the previous update, the issue now is with nav link is not highlighting the current page, it keeps highlight the index

--- Project_5/app/Http/Controllers/AppController.php
+++ Project_5/app/Http/Controllers/AppController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     public function index(){
-        return view('index');
+        return view('main_index');
     }
 
     public function cats(){
